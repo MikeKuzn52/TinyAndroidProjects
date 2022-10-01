bc1837485343d1edba2aee55050943558b5798f3
MikeKuzn52 <mikekuzn@mail.ru>
2022-10-01 22:46:17 +0300

    PickImageExample: Move image picker logic to external class

--- PickImageExample/app/src/main/java/com/mikekuzn/examle/pickimageexample/MainActivity.kt
+++ PickImageExample/app/src/main/java/com/mikekuzn/examle/pickimageexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mikekuzn.examle.pickimageexample
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
 import coil.load
 import com.mikekuzn.examle.pickimageexample.databinding.ActivityMainBinding
@@ -11,17 +12,26 @@ import com.mikekuzn.examle.pickimageexample.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
-    private val getImageUrl =
-        registerForActivityResult(ActivityResultContracts.GetContent()) {
-                imageUrl: Uri? ->
-            binding.image.load(imageUrl)
-        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.button.setOnClickListener {
-            getImageUrl.launch("image/*")
+            imagePicker.PickImage("image/*")
         }
+    }
+
+    private val imagePicker = ImagePicker(activityResultRegistry) { imageUrl ->
+        binding.image.load(imageUrl)
+    }
+}
+
+class ImagePicker(
+    private val activityResultRegistry: ActivityResultRegistry,
+    private val callBack: (imageUri:Uri?) -> Unit) {
+    private val getImageUrl =
+        activityResultRegistry.register("ImagePicker",ActivityResultContracts.GetContent(), callBack)
+    fun PickImage(req:String) {
+        getImageUrl.launch(req)
     }
 }
